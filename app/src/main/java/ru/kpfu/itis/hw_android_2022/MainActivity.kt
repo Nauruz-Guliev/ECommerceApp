cca98b63917087320db3231c6b4f5ece5081b6a7
nauruz <nauruz9248327@gmail.com>
2022-09-17 15:44:15 +0300

    initial commit

---
+++ app/src/main/java/ru/kpfu/itis/hw_android_2022/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.kpfu.itis.hw_android_2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
